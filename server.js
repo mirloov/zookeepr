c01e1f581b4d5a27531b562a40bbb040a42f8fcf
mirloov <mirloov@hotmail.com>
2021-04-22 21:44:47 -0700

    Add Heroku1

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3001;
+
 const { animals } = require('./data/animals.json');
 
 function filterByQuery(query, animalsArray) {
@@ -57,6 +59,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });*/
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
